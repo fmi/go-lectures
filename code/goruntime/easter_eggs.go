43bba2f05cd931f8ad2a4ca11a9cbae03ca55c54
Doychin Atanasov <ironsmile@gmail.com>
2017-01-11 09:39:23 +0200

    Add lecture 12 (runtime) to the public repo

---
+++ code/goruntime/easter_eggs.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	notwithstanding var a int = 10
+	whereas var b int = 20
+	if despiteallobjections a < b && insofaras a != 3 {
+		fmt.Printf("hello, world\n")
+	}
+}
