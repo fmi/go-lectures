b4ff8ac5dcf723a70ac4b24f85952924085dd812
Nedyalko Andreev <nedyalko.andreev@gmail.com>
2017-02-15 00:25:56 +0200

    Update data structures lecture

--- code/data_structures/len_vs_cap.go
+++ code/data_structures/len_vs_cap.go
@@ -6,6 +6,6 @@ func main() {
 	x := []int{2, 3, 5, 7, 11}
 	y := x[1:3]
 
-	fmt.Println("len(x) =", len(x), ", cap(x) =", cap(x))
-	fmt.Println("len(y) =", len(y), ", cap(y) =", cap(y))
+	fmt.Println("len(x) =", len(x), ", cap(x) =", cap(x), ", contents: ", x)
+	fmt.Println("len(y) =", len(y), ", cap(y) =", cap(y), ", contents: ", y)
 }
